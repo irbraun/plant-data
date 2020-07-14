4df8da57e53135b7e1e612179bacbf148860a71e
irbraun <irbraun@iastate.edu>
2020-07-14 15:03:12 -0500

    updating datasets

--- scripts/create_combined_dataset.py
+++ scripts/create_combined_dataset.py
@@ -14,9 +14,15 @@ from oats.utils.utils import save_to_pickle
 
 
 
+def truncate_string(text, char_limit):
+	truncated_text = text[:char_limit]
+	if len(text)>char_limit:
+		truncated_text = "{}...".format(truncated_text)
+	return(truncated_text)
 
 
-def save_combined_dataset_to_files(pickle_path, tsv_path, input_dir, *input_filenames):
+
+def save_combined_dataset_to_files(pickle_path, tsv_path, tsv_sample_path, input_dir, *input_filenames):
 
 	print("\nworking on creating", os.path.basename(pickle_path))
 	dataset = Dataset()
@@ -41,21 +47,34 @@ def save_combined_dataset_to_files(pickle_path, tsv_path, input_dir, *input_file
 	df = df.sort_values(by="id",inplace=False)
 	df.to_csv(tsv_path, sep="\t", index=False)
 
+	# Prepare a sample file from that whole dataset that makes it easy to understand what the context is.
+	df = df.head(100)
+	df["gene_names"] = df["gene_names"].map(lambda x: truncate_string(x, 20))
+	df["gene_synonyms"] = df["gene_synonyms"].map(lambda x: truncate_string(x, 20))
+	df["description"] = df["description"].map(lambda x: truncate_string(x, 100))
+	df.to_csv(tsv_sample_path, sep="\t", index=False)
+
+
+
+
+
+
+
 	print("done")
 
 
 reshaped_dir = "../reshaped_data"
 
 
-save_combined_dataset_to_files("../pickles/genes_texts_and_annotations.pickle", "../genes_texts_and_annotations.tsv", reshaped_dir, 
+save_combined_dataset_to_files("../pickles/genes_texts_annots.pickle", "../genes_texts_annots.tsv",  "../genes_texts_annots_sample.tsv", reshaped_dir, 
 	"oellrich_walls_phene_descriptions.csv", 
 	"oellrich_walls_phenotype_descriptions.csv", 
 	"oellrich_walls_annotations.csv",
 	"sgn_phenotype_descriptions.csv", 
 	"maizegdb_phenotype_descriptions.csv", 
-	"maizegdb_curated_go_annotations.csv",
+	#"maizegdb_curated_go_annotations.csv",
 	"tair_phenotype_descriptions.csv",
-	"tair_curated_go_annotations.csv", 
-	"tair_curated_po_annotations.csv"
+	#"tair_curated_go_annotations.csv", 
+	#"tair_curated_po_annotations.csv"
 	)
 
