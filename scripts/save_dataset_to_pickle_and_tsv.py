2e6e1b3fe0f5df85f348484fe8fd14d43468684c
irbraun <irbraun@iastate.edu>
2020-07-27 16:41:24 -0500

    updating preprocessing notebooks

--- scripts/save_dataset_to_pickle_and_tsv.py
+++ scripts/save_dataset_to_pickle_and_tsv.py
@@ -34,7 +34,7 @@ def save_combined_dataset_to_files(pickle_path, tsv_path, tsv_sample_path, input
 	# Saving a version of the dataset prior to merging based on gene names.
 	split_basename = os.path.basename(pickle_path).split(".")
 	unmerged_pickle_path = os.path.join(os.path.dirname(pickle_path), "{}_unmerged.{}".format(split_basename[0],split_basename[1]))
-	save_to_pickle(obj=dataset, path=unmerged_pickle_path)	
+	#save_to_pickle(obj=dataset, path=unmerged_pickle_path)	
 
 	# Saving the version of the dataset after merging based on gene names.
 	print("merging rows based on gene names...")
@@ -42,10 +42,13 @@ def save_combined_dataset_to_files(pickle_path, tsv_path, tsv_sample_path, input
 	dataset.filter_has_description()
 
 	# Write the dataset both to a pickle that can be loaded as an object and a tsv file for checking.
-	save_to_pickle(obj=dataset, path=pickle_path)
+	#save_to_pickle(obj=dataset, path=pickle_path)
 	df = dataset.to_pandas()
 	df = df.sort_values(by="id",inplace=False)
 	df.to_csv(tsv_path, sep="\t", index=False)
+
+	df.to_csv("/Users/irbraun/Desktop/test.csv", index=False)
+
 
 	# Prepare a sample file from that whole dataset that makes it easy to understand what the context is.
 	df = df.head(100)
@@ -55,6 +58,8 @@ def save_combined_dataset_to_files(pickle_path, tsv_path, tsv_sample_path, input
 	df["term_ids"] = df["term_ids"].map(lambda x: truncate_string(x, 60))
 	df.to_csv(tsv_sample_path, sep="\t", index=False)
 	print("done")
+
+	print(df[["gene_names","gene_synonyms"]].head(10))
 
 
 reshaped_dir = "../reshaped_data"
@@ -66,9 +71,9 @@ save_combined_dataset_to_files("../pickles/genes_texts_annots.pickle", "../genes
 	"oellrich_walls_annotations.csv",
 	"sgn_phenotype_descriptions.csv", 
 	"maizegdb_phenotype_descriptions.csv", 
-	"maizegdb_curated_go_annotations.csv",
+	#"maizegdb_curated_go_annotations.csv",
 	"tair_phenotype_descriptions.csv",
-	"tair_curated_go_annotations.csv", 
-	"tair_curated_po_annotations.csv"
+	#"tair_curated_go_annotations.csv", 
+	#"tair_curated_po_annotations.csv"
 	)
 
